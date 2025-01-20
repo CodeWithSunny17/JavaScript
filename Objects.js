a956c7e7767f84d098f0c3642006e1d0a86ac945
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-20 12:02:31 +0530

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,10 @@
+let obj = {
+name: "Sourav",
+age: 23,
+job: "Developer"
+};
+
+obj.name ="Sunny"              //we can change the value of any key of an object like this 
+console.log(obj.name);          //will print “Sunny”
+
+delete obj.age;                         //age will be deleted from the obj
