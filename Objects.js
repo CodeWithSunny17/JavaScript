f36b8ed38a986fe67356bd483050366e54ec6ea9
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-26 19:13:38 +0530

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -69,3 +69,46 @@ console.log(JSON.stringify(user))
 console.log(JSON.parse(storeObj))       //string is parsed back to object
 
 
+
+//JS also treats a string like an array
+const a= "sunny"
+console.log([...a])
+
+//OP: ['s', 'u', 'n', 'n', 'y']
+
+
+
+//object destructuring in JS
+const user ={
+  name: "sunny",
+  age:24,
+  fullName:{
+      first: "sunny",
+      last: "kumar"
+  }
+}
+
+const {name, age, fullName:{ first, last }} = user;  //fullNmae:{} is done in this for further destructuring
+console.log(first)
+
+
+
+//object referencing questions
+//DEEP COPY and SHALLOW COPY, expained here!
+const c = { name: "sunny" }
+const d = c;                      //SHALLOW COPY, when an object simply hold the reference of another object, we are not copying the objects here
+const e = {...c}                  //DEEP COPY, this is how we can truly clone(copy) an object
+const f = Object.assign({},c)     //DEEP COPY, another example of deep copy
+
+d.name= "aditya"
+
+console.log(d)  //OP: "sunny"
+
+
+
+//both will give false because both element in each comparison has difference reference
+console.log([1] === [1])     //false
+console.log([1] == [1])      //false
+
+
+
