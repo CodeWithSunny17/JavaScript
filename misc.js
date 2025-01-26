14c2f19f6f16526c0e01225161eadcd477e6c8b2
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-27 01:41:59 +0530

    Create misc.js

---
+++ misc.js
@@ -0,0 +1,10 @@
+//simply making a key value pair using two different arrays
+array = [0,1,2,3]
+array2= ["a", "b", "c", "d"]
+
+var obj={}
+for(let i=0; i<4 ;i++){
+    obj={...obj, [array[i]] : array2[i]};
+}
+
+console.log(obj)
