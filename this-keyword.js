506a7c747c6886d942e3e86b656e8dd457b11f05
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-26 20:12:27 +0530

    Update this-keyword.js

--- this-keyword.js
+++ this-keyword.js
@@ -34,3 +34,28 @@ let user={
 
 user.innerObj.getDetails()    //roadside coder, here this keyword refer to its parent object innerObj which has name key
 user.innerObj.arrowFun()      //undefined, this will point to window object which does not have name variable
+
+
+
+
+
+
+// example 4
+const calc={
+    total:0,
+    add(a){
+        this.total=this.total+a;
+        return this;
+    },
+    subtract(b){
+        this.total=this.total-b;
+        return this;
+    },
+    multiply(c){
+        this.total=this.total*c
+        return this;
+    }
+}
+
+const result = calc.add(5).subtract(2).multiply(6)
+console.log(result.total)        //OP: 18
