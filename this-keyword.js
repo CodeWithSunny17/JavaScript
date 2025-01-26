d99c2636f106813915bb0de1d12152ebcc1316a4
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-26 19:55:11 +0530

    Update this-keyword.js

--- this-keyword.js
+++ this-keyword.js
@@ -1,1 +1,36 @@
+//this keyword is used to refer an object 
+//(IMPLICIT Binding)
 
+//example 1
+let user={
+    name : "sunny",
+    age:24,
+    innerObj:{
+        
+        getDetails(){
+            console.log(this.name)
+        }
+    }
+}
+
+user.innerObj.getDetails()    //undefined, because this keyword will refer to its parents object i.e. innerObj{} which does not have name key
+
+
+
+// example2
+let user={
+    name : "sunny",
+    age:24,
+    innerObj:{
+        name: "roadside coder",
+        getDetails(){
+            console.log(this.name)
+        },
+        arrowFun: () =>{
+            console.log(this.name)
+        }
+    }
+}
+
+user.innerObj.getDetails()    //roadside coder, here this keyword refer to its parent object innerObj which has name key
+user.innerObj.arrowFun()      //undefined, this will point to window object which does not have name variable
