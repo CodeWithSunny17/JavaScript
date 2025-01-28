0f34e2121389e63a338d5d3dc373f65dab10fbfd
Sunny Kumar <99110329+CodeWithSunny17@users.noreply.github.com>
2025-01-29 00:54:37 +0530

    Update debouncing-throattling.js

--- debouncing-throattling.js
+++ debouncing-throattling.js
@@ -1,1 +1,10 @@
-//Debouncing
+// Note: this topic can be better explained in React
+
+// Debouncing: Debouncing is a technique in programming that helps improve the performance of web applications by controlling the frequency at which time-consuming tasks are triggered. 
+// If a task is triggered too often—like when a user types quickly or rapidly clicks a button—it can lead to performance issues. 
+// Debouncing provides a solution by limiting how frequently a function can be executed.
+
+
+// Throattling: Throttling is a technique used to limit the number of times a function can be executed in a given time frame. 
+// It’s extremely useful when dealing with performance-heavy operations, such as resizing the window or scrolling events, where repeated triggers can lead to performance issues.
+
